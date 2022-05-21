Project-Awaken/android_frameworks_base

SystemUI: Update mobile data icon instantly when toggled
Change-Id: Ic3cb0749b778d9caf2493757ac3580a743e53c19
Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarSignalPolicy.java
@@ -18,6 +18,7 @@ package com.android.systemui.statusbar.phone;
 
 import android.content.Context;
 import android.os.Handler;
+import android.provider.Settings;
 import android.telephony.SubscriptionInfo;
 import android.util.ArraySet;
 import android.util.Log;
@@ -48,6 +49,9 @@ public class StatusBarSignalPolicy implements NetworkControllerImpl.SignalCallba
         SecurityController.SecurityControllerCallback, Tunable {
     private static final String TAG = "StatusBarSignalPolicy";
     private static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);
+
+    private static final String USE_OLD_MOBILETYPE =
+            "system:" + Settings.System.USE_OLD_MOBILETYPE;
 
     private final String mSlotAirplane;
     private final String mSlotMobile;
@@ -113,8 +117,7 @@ public class StatusBarSignalPolicy implements NetworkControllerImpl.SignalCallba
                 mContext.getString(com.android.internal.R.string.status_bar_call_strength);
         mActivityEnabled = mContext.getResources().getBoolean(R.bool.config_showActivity);
 
-
-        tunerService.addTunable(this, StatusBarIconController.ICON_HIDE_LIST);
+        tunerService.addTunable(this, StatusBarIconController.ICON_HIDE_LIST, USE_OLD_MOBILETYPE);
         mNetworkController.addCallback(this);
         mSecurityController.addCallback(this);
     }
@@ -152,6 +155,10 @@ public class StatusBarSignalPolicy implements NetworkControllerImpl.SignalCallba
 
     @Override
     public void onTuningChanged(String key, String newValue) {
+        if (USE_OLD_MOBILETYPE.equals(key)) {
+            mNetworkController.removeCallback(this);
+            mNetworkController.addCallback(this);
+        }
         if (!StatusBarIconController.ICON_HIDE_LIST.equals(key)) {
             return;
         }

LessaospOS/frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarSignalPolicy.java
@@ -19,6 +19,7 @@ package com.android.systemui.statusbar.phone;
 import android.annotation.NonNull;
 import android.content.Context;
 import android.os.Handler;
+import android.provider.Settings;
 import android.telephony.SubscriptionInfo;
 import android.util.ArraySet;
 import android.util.Log;
@@ -49,6 +50,9 @@ public class StatusBarSignalPolicy implements SignalCallback,
         SecurityController.SecurityControllerCallback, Tunable {
     private static final String TAG = "StatusBarSignalPolicy";
     private static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);
+
+    private static final String USE_OLD_MOBILETYPE =
+            "system:" + Settings.System.USE_OLD_MOBILETYPE;
 
     private final String mSlotAirplane;
     private final String mSlotMobile;
@@ -119,7 +123,7 @@ public class StatusBarSignalPolicy implements SignalCallback,
             return;
         }
         mInitialized = true;
-        mTunerService.addTunable(this, StatusBarIconController.ICON_HIDE_LIST);
+        mTunerService.addTunable(this, StatusBarIconController.ICON_HIDE_LIST, USE_OLD_MOBILETYPE);
         mNetworkController.addCallback(this);
         mSecurityController.addCallback(this);
     }
@@ -157,6 +161,10 @@ public class StatusBarSignalPolicy implements SignalCallback,
 
     @Override
     public void onTuningChanged(String key, String newValue) {
+        if (USE_OLD_MOBILETYPE.equals(key)) {
+            mNetworkController.removeCallback(this);
+            mNetworkController.addCallback(this);
+        }
         if (!StatusBarIconController.ICON_HIDE_LIST.equals(key)) {
             return;
         }